058c494a6869d05b38c688aa25ae99098246818a
martafil <marta.dianna@gmail.com>
2016-12-11 19:04:30 +0200

    Added Modules II aiml files and session test

--- Module-I/Bot Brain Prototype/bot.py
+++ Module-I/Bot Brain Prototype/bot.py
@@ -7,6 +7,9 @@ kernel = aiml.Kernel()
 kernel.learn("std-startup.xml")
 kernel.respond("load aiml b")
 
+sessionID = 12345
+
 # kernel now ready for use
 while True:
     print kernel.respond(raw_input("Enter your message >> "))
+    kernel.setPredicate("dog", kernel.getPredicate("dog"), sessionID)
