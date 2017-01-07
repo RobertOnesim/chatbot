bc7b8ebc0e8ec2aac9577645b7b0b04e71078851
CatalinaBaincescu <catalina.baincescu@gmail.com>
2017-01-07 21:52:03 +0200

    added the route update the predicates for user.

--- Module-II/project/controllers/userController.js
+++ Module-II/project/controllers/userController.js
@@ -2,6 +2,7 @@ var userController = function (urlDB) {
 
     var mongodb = require('mongodb').MongoClient;
     var objectId = require('mongodb').ObjectID;
+    var lodash = require('lodash');
 
     var getUsers = function (req, res) {
         mongodb.connect(urlDB)
@@ -32,11 +33,14 @@ var userController = function (urlDB) {
         var errors = req.validationErrors();
         if (errors) {
             res.status(400);
-            res.send({data: null, error: { statusCode:400, errorMessage: errors}});
+            res.send({data: null, error: {statusCode: 400, errorMessage: errors}});
             return;
         }
 
-        var newUser = req.body;
+        var newUser = {
+            username: req.body.username,
+            password: req.body.password
+        };
         mongodb.connect(urlDB)
             .then(function (db) {
                 var collection = db.collection('user');
@@ -44,7 +48,13 @@ var userController = function (urlDB) {
                     .then(function (result) {
                         if (result) {
                             res.status(400);
-                            res.send({data: null, error: { statusCode:400, errorMessage: {param: "username", msg: "This username already exists"}}});
+                            res.send({
+                                data: null,
+                                error: {
+                                    statusCode: 400,
+                                    errorMessage: {param: "username", msg: "This username already exists"}
+                                }
+                            });
                         } else {
                             collection.insertOne(newUser)
                                 .then(function (results) {
@@ -54,18 +64,18 @@ var userController = function (urlDB) {
                                 })
                                 .catch(function (err) {
                                     res.status(400);
-                                    res.send({data: null, error: { statusCode:400, errorMessage: err}});
+                                    res.send({data: null, error: {statusCode: 400, errorMessage: err}});
                                 });
                         }
                     })
                     .catch(function (err) {
                         res.status(400);
-                        res.send({data: null, error: { statusCode:400, errorMessage: err}});
+                        res.send({data: null, error: {statusCode: 400, errorMessage: err}});
                     });
             })
             .catch(function (err) {
                 res.status(500);
-                res.send({data: null, error: { statusCode:500, errorMessage: err}});
+                res.send({data: null, error: {statusCode: 500, errorMessage: err}});
             });
     };
 
@@ -76,35 +86,101 @@ var userController = function (urlDB) {
         var errors = req.validationErrors();
         if (errors) {
             res.status(400);
-            res.send({data: null, error: { statusCode:400, errorMessage: errors}});
+            res.send({data: null, error: {statusCode: 400, errorMessage: errors}});
             return;
         }
+        var user = {
+            username: req.body.username,
+            password: req.body.password
+        };
         mongodb.connect(urlDB)
             .then(function (db) {
                 var collection = db.collection('user');
-                collection.findOne(req.body)
+                collection.findOne(user)
                     .then(function (result) {
                         if (result) {
                             res.status(200);
                             res.send({data: result, error: null});
                         } else {
                             res.status(404);
-                            res.send({data: null, error: { statusCode:404, errorMessage: 'User not found'}});
+                            res.send({data: null, error: {statusCode: 404, errorMessage: 'User not found'}});
                         }
                         db.close();
                     })
                     .catch(function (err) {
                         res.status(400);
-                        res.send({data: null, error: { statusCode:400, errorMessage: err}});
+                        res.send({data: null, error: {statusCode: 400, errorMessage: err}});
                     });
             })
             .catch(function (err) {
                 res.status(400);
-                res.send({data: null, error: { statusCode:400, errorMessage: err}});
+                res.send({data: null, error: {statusCode: 400, errorMessage: err}});
             });
     };
 
     var updateUser = function (req, res) {
+        req.checkParams('username', 'Username is required').notEmpty();
+        req.checkBody('predicates', 'Predicates are required').notEmpty();
+
+        var errors = req.validationErrors();
+        if (errors) {
+            res.status(400);
+            res.send({data: null, error: {statusCode: 400, errorMessage: errors}});
+            return;
+        }
+        mongodb.connect(urlDB)
+            .then(function (db) {
+                var collection = db.collection('user');
+                collection.findOne({username: req.params.username})
+                    .then(function (result) {
+                        if (result) {
+                            if (result.predicates === undefined) {
+                                result.predicates = req.body.predicates;
+                            } else {
+                                for (var atr in req.body.predicates) {
+                                    if (lodash.isArray(req.body.predicates[atr])) {
+                                        if (result.predicates[atr]) {
+                                            result.predicates[atr] = lodash.union(result.predicates[atr], req.body.predicates[atr]);
+                                        }
+                                        else {
+                                            result.predicates[atr] = req.body.predicates[atr];
+                                        }
+                                    } else if (result.predicates[atr]) {
+                                        result.predicates[atr] = req.body.predicates[atr];
+                                    }
+                                    else {
+                                        result.predicates[atr] = req.body.predicates[atr];
+                                    }
+                                }
+                            }
+                            collection.updateOne({username: req.params.username}, result)
+                                .then(function (results) {
+                                    res.status(200);
+                                    res.send({data: result, error: null});
+                                    db.close();
+                                })
+                                .catch(function () {
+                                    res.status(400);
+                                    res.send({
+                                        data: null,
+                                        error: {statusCode: 400, errorMessage: "The object was the same"}
+                                    });
+                                });
+                        } else {
+                            res.status(404);
+                            res.send({data: null, error: {statusCode: 404, errorMessage: 'User not found'}});
+                        }
+                        db.close();
+                    })
+                    .catch(function (err) {
+                        res.status(400);
+                        res.send({data: null, error: {statusCode: 400, errorMessage: err}});
+                    });
+            })
+            .catch(function (err) {
+                res.status(400);
+                res.send({data: null, error: {statusCode: 400, errorMessage: err}});
+            });
 
     };
 
