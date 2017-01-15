ecaab05652636ddb40ea5a0170584c081e32cef7
Catalina <catalina.baincescu@gmail.com>
2017-01-15 20:17:45 +0200

    added SPARQLWrapper module from python

---
+++ Module-II/project/dbpedia.py
@@ -0,0 +1,23 @@
+
+# https://github.com/RDFLib/sparqlwrapper
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+
+input = 'Albert_Einstein'
+# input = 'Einstein'  # not good, result nulls
+
+sparql.setQuery("""
+     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        SELECT  ?label
+                        WHERE {<http://dbpedia.org/resource/""" + input + """>
+                        rdfs:comment  ?label
+                        filter langMatches(lang(?label),"en")}
+""")
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+for result in results["results"]["bindings"]:
+    print(result["label"]["value"])
+
